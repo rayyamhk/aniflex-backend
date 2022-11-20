9856518cf764e754400c04db91c00b297a8af536
rayyamhk <rayyamhk@gmail.com>
2022-11-20 17:42:38 -0500

    clean up unnecessary files

--- src/constants.ts
+++ src/constants.ts
@@ -7,8 +7,6 @@ export const IMAGES_STORAGE = 'aniflex-images';
 export const VIDEOS_STORAGE = 'aniflex-videos';
 
 // Regular Expressions
-export const UUID_REGEX =
-  /^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$/i;
 export const MEDIA_KEY_REGEX = /^[0-9a-f]{16}$/i;
 export const IMAGE_KEY_SRC_REGEX = /^\/images\/[0-9a-f]{16}$/;
 export const IMAGE_MIMETYPE_REGEX = /^image\/(jpeg|png|webp)$/i;
